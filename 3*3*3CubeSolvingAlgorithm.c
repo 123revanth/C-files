7092a140834969571032e8f560dffcbf7912aba8
Revanth jallipalli <81552258+123revanth@users.noreply.github.com>
2022-10-05 17:16:11 +0530

    Create 3*3*3CubeSolvingAlgorithm.c

---
+++ 3*3*3CubeSolvingAlgorithm.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+
+    int sideone [3][3] = {{0, 0, 0}, {0, 0 , 0}, {0, 0, 0}};
+
+    printf("include the first side");
+    
+    for(int i = 0; i <= 2; i++){
+        for(int j = 0; j <= 2; j++){
+            scanf("%d", &sideone[i][j]);
+        }
+    }
+
+
+    printf("include the second side");
+    
+    for(int i = 0; i <= 2; i++){
+        for(int j = 0; j <= 2; j++){
+            scanf("%d", &sideone[i][j]);
+        }
+    }
+
+
+    printf("include the third side");
+    
+    for(int i = 0; i <= 2; i++){
+        for(int j = 0; j <= 2; j++){
+            scanf("%d", &sideone[i][j]);
+        }
+    }
+
+
+    printf("include the fourth side");
+    
+    for(int i = 0; i <= 2; i++){
+        for(int j = 0; j <= 2; j++){
+            scanf("%d", &sideone[i][j]);
+        }
+    }
+
+    printf("include the fifth side");
+    
+    for(int i = 0; i <= 2; i++){
+        for(int j = 0; j <= 2; j++){
+            scanf("%d", &sideone[i][j]);
+        }
+    }
+
+    printf("include the sixth side");
+    
+    for(int i = 0; i <= 2; i++){
+        for(int j = 0; j <= 2; j++){
+            scanf("%d", &sideone[i][j]);
+        }
+    }
+
+     printf("The included first side \n");
+
+    for(int i = 0; i <= 2; i++){
+        for(int j = 0; j <= 2; j++){
+            printf("%d ", sideone[i][j]);
+        }
+        printf("\n");
+    }
+
+        printf("The included second side \n");
+
+    for(int i = 0; i <= 2; i++){
+        for(int j = 0; j <= 2; j++){
+            printf("%d ", sideone[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("The included third side \n");
+
+    for(int i = 0; i <= 2; i++){
+        for(int j = 0; j <= 2; j++){
+            printf("%d ", sideone[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("The included fourth side \n");
+
+    for(int i = 0; i <= 2; i++){
+        for(int j = 0; j <= 2; j++){
+            printf("%d ", sideone[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("The included fifth side \n");
+
+    for(int i = 0; i <= 2; i++){
+        for(int j = 0; j <= 2; j++){
+            printf("%d ", sideone[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("The included sixth side \n");
+
+    for(int i = 0; i <= 2; i++){
+        for(int j = 0; j <= 2; j++){
+            printf("%d ", sideone[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+
+}
